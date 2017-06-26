afc606795d48087a6f24ed7cedfe6d3689f695fb
Grzegorz-PC\Grzegorz <grzesiu15432@gmail.com>
2017-06-26 22:02:19 +0200

    03_ Added  Action 'New' to Movie and Customer + Views + Validation to CustomerForm

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -5,18 +5,40 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/Random
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+                _context=new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
             return View(movies);
         }
 
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
@@ -34,14 +56,52 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
-
-        private IEnumerable<Movie> GetMovies()
+        public ActionResult New()
         {
-            return new List<Movie>
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
             {
-                new Movie{Id=1,Name="Shrek!"},
-                new Movie{Id=2,Name="Wall-e"}
+                Genres = genres
             };
+
+            return View("MovieForm",viewModel);
+        }
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+               var movieInDb= _context.Movies.Single(m => movie.Id == m.Id);
+                movieInDb.Genre = movie.Genre;
+                movieInDb.Name = movie.Name;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
+
+            _context.SaveChanges();
+            
+
+            return RedirectToAction("Index", "Movies");
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == Id);
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres
+            };
+            return View("MovieForm", viewModel);
         }
     }
 }
